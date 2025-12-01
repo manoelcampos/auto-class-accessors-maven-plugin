fd1d8f82b4ab9a855104d9182f71d0482d3bb00e
Manoel Campos <manoelcampos@gmail.com>
2025-12-01 18:19:05 -0300

    Format docs
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- src/main/java/io/github/manoelcampos/accessors/InstanceFieldMatcher.java
+++ src/main/java/io/github/manoelcampos/accessors/InstanceFieldMatcher.java
@@ -11,7 +11,8 @@ import static net.bytebuddy.matcher.ElementMatchers.*;
 
 /**
  * An {@link ElementMatcher} that matches instance fields that are public and non-static.
- * It's used to intercept reads and writes to such fields in order to replace them by the respective accessor method calls.
+ * It's used to intercept reads and writes to such fields in order to replace them by
+ * the respective accessor method calls.
  *
  * @author Manoel Campos
  * @see ClassAccessorInstrumentationPlugin
